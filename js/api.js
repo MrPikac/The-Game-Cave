9366d36811d82df8afee1a8230bf9ca865898a1f
MrPikac <milos.novisad@icloud.com>
2019-04-21 13:56:05 +0200

    irnero

--- js/api.js
+++ js/api.js
@@ -4,14 +4,14 @@ var req = new XMLHttpRequest();
 req.onload = function() {
   var res = JSON.parse(req.responseText);
   
-  var all = res.products;
+  var cod = res.products.fps.cod;
   
-  for (let i=0; i < all.length; i++) {
+  for (let i=0; i < fps.length; i++) {
     var ele = document.createElement('p');
-    var text = document.createTextNode(all[i].name);
+    var text = document.createTextNode(fps[i].name);
     ele.appendChild(text);
     
-    ele.classList.add('red');
+//     ele.classList.add('red');
     
     
     document.querySelector('span').appendChild(ele);

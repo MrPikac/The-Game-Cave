01bf495f588082b92fd771cf524c3efb17e20db9
MrPikac <milos.novisad@icloud.com>
2019-04-21 06:41:07 +0200

    gnrngi# Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-main
+function validate(){
+    var name = document.getElementById("name").value;
+    if (name === "") {
+        alert('The name is required');
+        return false;
+    }
+    if (name === Number) {
+        alert('The name is not a number...');
+        return false;
+    }
+    
+
+    var age = document.getElementById("age").value;
+    if (age === "") {
+        alert('The age is required');
+        return false;
+    }
+    return true;
+    
+}

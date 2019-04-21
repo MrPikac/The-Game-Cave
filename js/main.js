48f8948b7781df7898de16fdda865f75e56e7c27
MrPikac <milos.novisad@icloud.com>
2019-04-21 10:07:25 +0200

    going to do smth

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// VALIDATION
+
 function validate(){
     var name = document.getElementById("name").value;
     if (name === "") {
@@ -26,3 +28,26 @@ function validate(){
     
     return true
 }
+
+
+
+
+// ANIMATION OF SVG
+
+var looper;
+var degrees = 0;
+function rotateAnimation(el,speed) {
+    var elem = document.getElementById(el);
+    elem.style.transform = "rotate("+degrees+"deg)";
+    looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
+    degrees++;
+    if(degrees > 359) {
+        degrees = 1;
+    }
+}
+
+
+
+
+
+rotateAnimation("anim",20);
